c3ee462873d35760d30c8fb812d3ea1e7918b675
Sanjeev Kumar Mishra <sanjusoftware@gmail.com>
2010-12-26 21:13:46 +0530

    first commit to the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Techyhire::Application.config.secret_token = '19be55369e9015a4dd0e5ca3b3f681e579bf080cd304574fb0ff08a9b91f01e45f3abe1149b05efd5d0e07016a1b9fbdec2e36acea64da3e4540e2ff3e21f709'
